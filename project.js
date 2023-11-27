a6a5d7ead10e7e9db329c53fe9d2a5f7cd6fe784
asifalam515 <asibulalam515@gmail.com>
2023-11-28 03:42:05 +0600

    card done

--- project.js
+++ project.js
@@ -47,25 +47,43 @@ data.forEach((singleData)=>{
     // console.log(singleData);
 })
 
-// load data
-const categoryContainer = document.getElementById("categories-ul");
-categoryContainer.innerHTML = "";
-data?.forEach((category)=>{
-    console.log(category);
-    const div = document.createElement("div");
+const videoContainer = document.getElementById("video-container");
+videoContainer.innerHTML = "";
+data?.forEach((video) =>{
+    const div=document.createElement("div")
+    div.classList.add("col-3","m-2","text-center")
     div.innerHTML=`
- ]<div class="card" ">
- <img src="${category.thumbnail}" class="card-img-top img-fluid" alt="...">
- <div class="card-body">
-   <h5 class="card-title">Card title</h5>
-   <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-   <a href="#" class="btn btn-primary">Go somewhere</a>
- </div>
-</div>
-    
-    `
-    categoryContainer.appendChild(div);
+  <div class="container m-2">
+  <div class="card style="width: 18rem;"">
 
+  <div class="img" >
+  <img src="${video.thumbnail}" class="card-img-top" alt="...">
+  </div>
+
+
+<div class="d-flex justify-content-start">
+<span class="w-100"> <img class="w-25 rounded-5 img-fluid" src=${video?.authors[0].profile_picture} /></span>
+<h6 class="card-text"> ${video.title}</h6> 
+</div>
+
+  <div class="name-verification d-flex gap-2 justify-content-center">
+  <p class="">${video?.authors[0].profile_name}</p>
+  <span>${
+    video?.authors[0].verified
+      ? '<img class="img-fluid" src="./images/icons8-verified-30.png" alt="" />'
+      : ""
+  }
+  </span>
+  </div>
+
+
+  
+  </div>
+  
+  </div>
+
+    `
+    videoContainer.appendChild(div)
 })
 
 }
@@ -74,3 +92,38 @@ data?.forEach((category)=>{
 
 
 loadCategories()
+
+
+
+// extra
+function formatTime(seconds) {
+    if (seconds < 60) {
+      return `${seconds} seconds ago`;
+    } else if (seconds < 3600) {
+      const minutes = Math.floor(seconds / 60);
+      return `${minutes} ${minutes === 1 ? "minute" : "minutes"} ago`;
+    } else {
+      const hours = Math.floor(seconds / 3600);
+      const remainingMinutes = Math.floor((seconds % 3600) / 60);
+      return `${hours} ${hours === 1 ? "hour" : "hours"} ${remainingMinutes} ${
+        remainingMinutes === 1 ? "minute" : "minutes"
+      } ago`;
+    }
+  }
+
+
+  function sortViews(data, options) {
+    const sortedData = data.sort((a, b) => {
+      const aViews = parseInt(a.others.views);
+      const bViews = parseInt(b.others.views);
+      if (options) {
+        console.log(options);
+        return bViews - aViews;
+      } else {
+        console.log(options);
+        return aViews - bViews;
+      }
+    });
+    return sortedData;
+  }
+  
